bab3f66d14a61ad01465ad1ca59695cbc5a8b714
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-04-23 17:18:54 +0600

    adde coffee to the database using post method including sweet alert & form reset

--- coffee-store-client/src/pages/Add/AddCoffee.jsx
+++ coffee-store-client/src/pages/Add/AddCoffee.jsx
@@ -1,10 +1,44 @@
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AddCoffee = () => {
 
   const handleAddCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
     
+    const name = form.name.value;
+    const chef = form.chef.value;
+    const price = form.price.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {name, chef, price, quantity, supplier, taste, category, details, photo};
+
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers:{
+        "content-type": "application/json"
+      },
+      body:JSON.stringify(newCoffee)
+    }).then(res=>res.json()).then(data=>{
+      if(data.insertedId){
+        Swal.fire({
+          text: 'Coffee Added Successfully',
+          icon: 'success',
+          iconColor: '#D1B38D',
+          color: '#331A15',
+          confirmButtonText: 'Cool',
+          confirmButtonColor: '#D1B38D',
+        })
+        form.reset();
+      }
+    })
   } 
   return (
     <div>
